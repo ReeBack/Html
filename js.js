f9a92a52112d0b92298287378e2c2196e20f0afc
ReeBack <54481070+ReeBack@users.noreply.github.com>
2020-07-24 20:25:14 -0400

    add

---
+++ js.js
@@ -0,0 +1,18 @@
+setTimeout(hi, 5000)
+function hi() {
+  alert("hi");
+}
+console.log(2);
+console.log(1);
+
+  var cat = 1;
+
+  while(cat == 1) {
+    document.write("Hi cat");
+    cat = cat + 1;
+  }
+document.write("Hi do");
+
+if (i = 1, cat > 5, i++) {
+  console.log(1);
+}
