559d1bc566cf077fc3f8e53daab3e4b15e3e3972
Reback <54481070+ReeBack@users.noreply.github.com>
2020-08-19 18:55:24 -0400

    add

--- js.js
+++ js.js
@@ -1,6 +1,27 @@
-function games(name) {
-    name = prompt("Name?")
-    console.log(name + " is a egg.");
+var name = "";
+var gus;
+var hat = 0;
+var six = false;
+
+
+function games() {
+    name = prompt("Why is a ");
+    console.log("Why is a " + name);
+
+    while (six == false) {
+        hat = hat + 1;
+        gus = prompt("Two Three...")
+
+        if (hat == 3) {
+            console.log("War");
+
+            six = true;
+        }
+    }
 }
 
-games();
+games(name);
+
+var one = ["one", "two"];
+console.log(one[1]);
+console.log( one.length );
