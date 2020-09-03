1298369b9f795646d72cffd60c7a412b730ca200
Reback <54481070+ReeBack@users.noreply.github.com>
2020-09-02 20:13:06 -0400

    add

--- js.js
+++ js.js
@@ -1,27 +1,7 @@
-var name = "";
-var gus;
-var hat = 0;
-var six = false;
-
-
-function games() {
-    name = prompt("Why is a ");
-    console.log("Why is a " + name);
-
-    while (six == false) {
-        hat = hat + 1;
-        gus = prompt("Two Three...")
-
-        if (hat == 3) {
-            console.log("War");
-
-            six = true;
-        }
-    }
+var chevy = {
+    color: "red",
+    year: 1957,
+    min: "one"
 }
 
-games(name);
-
-var one = ["one", "two"];
-console.log(one[1]);
-console.log( one.length );
+console.log(chevy);
